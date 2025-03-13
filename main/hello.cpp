e18c7cb941e489d4bed5ff3abd66315a989e9451
N Pranav kumar <pranavnpk@gmail.com>
2025-03-13 15:58:00 +0530

    Describe this commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
+int main(void)
+{
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Jenkins";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
